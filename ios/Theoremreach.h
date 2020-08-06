61b4c0cf39f407ea9f1a89bad44b8fff7b2a2392
Tanguy de La Villegeorges <tvillegeorges@gmail.com>
2020-08-06 10:31:31 +0200

    integrate for ios

--- ios/Theoremreach.h
+++ ios/Theoremreach.h
@@ -1,5 +1,17 @@
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
 
-@interface Theoremreach : NSObject <RCTBridgeModule>
+#if __has_include(<React/RCTEventEmitter.h>)
+#import <React/RCTEventEmitter.h>
+#else
+#import "RCTEventEmitter.h"
+#endif
+
+#import <TheoremReach/TheoremReach.h>
+
+@interface RNTheoremreach : RCTEventEmitter <RCTBridgeModule>
 
 @end
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NativeModules } from 'react-native';
-
-type TheoremreachType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
-const { Theoremreach } = NativeModules;
-
-export default Theoremreach as TheoremreachType;
